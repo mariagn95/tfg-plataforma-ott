01e95325e56afeb53c88bb68bd41e3c7171126f0
mariagn95 <mariaagn95@gmail.com>
2026-05-27 15:16:41 +0200

    Modify the file

--- topo_sdn_video_streaming_dynamic.py
+++ topo_sdn_video_streaming_dynamic.py
@@ -40,7 +40,7 @@ def run():
     h2.cmd('xterm -T "CLIENTE DINAMICO h2" -e "python3 cliente.py; bash" &')
     
     # Espera inicial para permitir que el cliente arranque y llene buffer
-    print("[INFO] Esperando 15 segundos para estabilizar la reproducción inicial")
+    print("[INFO] Esperando 15 segundos para estabilizar la reproducción inicial...")
     sleep(15)
     
     # --- INICIO DEL CICLO DE VARIACIÓN DE RED ---
